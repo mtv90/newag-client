9732e76e5df3c125d13002c806c1f6092faf03ce
Maik Tran van <m_tv@gmx.de>
2019-05-17 16:04:15 +0200

    ausbau des frontends

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Axios from 'axios';
 
 import Routes from './routes/routes';
 import Header from './components/Header';
@@ -13,10 +12,7 @@ export default class App extends React.Component {
     }
   }
   componentDidMount(){
-    Axios.get('https://newag-app.herokuapp.com/api/termins')
-    // Axios.get('http://localhost:8080/api/getAllPatient')
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
+
   }
   render(){
     return (
